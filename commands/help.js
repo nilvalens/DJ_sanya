76ec93982c7c97baba43ec9e59d1eee918543eb5
nilvalens <157700744+nilvalens@users.noreply.github.com>
2024-01-25 18:07:59 +0400

    help.js

--- commands/help.js
+++ commands/help.js
@@ -57,7 +57,24 @@ module.exports = {
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
        .setImage('https://cdn.discordapp.com/attachments/367353824819544065/1200069099691196536/standard.png?ex=65c4d671&is=65b26171&hm=da8544dab294d6ec523aaadb5d21ad7ae4b990fe091c4800f113a1bcbc90130c&')
+const button1 = new ButtonBuilder()
+        .setLabel('YouTube')
+        .setURL('')
+        .setStyle(ButtonStyle.Link);
 
+      const button2 = new ButtonBuilder()
+        .setLabel('Discord')
+        .setURL('')
+        .setStyle(ButtonStyle.Link);
+
+      const button3 = new ButtonBuilder()
+        .setLabel('Code')
+        .setURL('')
+        .setStyle(ButtonStyle.Link);
+
+      const row = new ActionRowBuilder()
+        .addComponents(button1, button2, button3);
+   
       interaction.reply({
         embeds: [musicCommandsEmbed, basicCommandsEmbed],
         components: [row]
